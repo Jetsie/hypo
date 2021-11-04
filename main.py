a2460acdd061254763485b76dca15a0d7b1b82fe
Jet <84949210+Jetsie@users.noreply.github.com>
2021-11-04 07:32:16 -1000

    Add images folder.

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import pygame
-import sys
+import sys, time
 from pygame.locals import *
 
 import isometric
 
-
 #
 pygame.font.init()
-font = pygame.font.SysFont("Arial", 8)
+font = pygame.font.SysFont("Arial", 30)
 #
 
 res = (900, 900)
@@ -56,42 +55,40 @@ while True:
 		for y, row in enumerate(layer):
 			for x, tile in enumerate(row):
 				if type(level.textures[tile]) == pygame.Surface:
-					iso.display.blit(level.textures[tile], ((x * half_width - y * half_width) + offset[0] - half_width,
-															(x * quarter_width + y * quarter_width) + offset[1] - (
-																		z * 14) + half_width))
-					iso.display.blit(font.render(f'{x}.{y}', False, (255, 255, 255)), ((x * half_width - y * half_width) + offset[0] - half_width, (x * quarter_width + y * quarter_width) + offset[1] - (
-																		z * 14) + half_width))
+					ptile = ((x * half_width - y * half_width) + offset[0] - half_width, (x * quarter_width + y * quarter_width) + offset[1] - (z * 14) + half_width)
+					ppos = ((player[0] * half_width - player[1] * half_width) + offset[0] - sprite_res[0], (player[0] * quarter_width + player[1] * quarter_width) + offset[1] - (player[2] * 14) - half_width)
+
+					point = False
+					if (player[0] < x and player[1] < y and player[2] < z) and (player[0]+1 > x and player[1]+1 > y and player[2]+2 > z):
+						iso.display.blit(current, ppos)
+						iso.display.blit(level.textures[tile], ptile)
+						point = True
+					else:
+						iso.display.blit(level.textures[tile], ptile)
+
+					if not point:
+						iso.display.blit(current, ppos)
+				# if player[0] in range(x, x+1):
+
+	# pygame.draw.circle(iso.display, (255, 0, 0), ((player[0] * half_width - player[1] * half_width) + offset[0] - half_width, (player[0] * quarter_width + player[1] * quarter_width) + offset[1] - (player[2] * 14) + half_width), 2)
+	# iso.display.blit(current, ((player[0] * half_width - player[1] * half_width) + offset[0] - half_width, (player[0] * quarter_width + player[1] * quarter_width) + offset[1] - (player[2] * 14) + half_width))
+
 	# This line is where the camera follows the player, add cam effects here
 	offset = (player[0] * -1 + pix_res[0] / 2, player[1] * -1 + pix_res[1] / 2)
 
-	print(int(player[0]/half_width), int(player[1]/quarter_width))
+	# print((int(player[0]), int(player[1]), int(player[2])))
 
-	iso.display.blit(current, (offset[0] + player[0] - half_width, offset[1] + player[1] - 12))
-	# pygame.draw.circle(iso.display, (0, 20, 200), (offset[0] + player[0], offset[1] + player[1]), 2)  # Draw player
-
-	speed = 60
-	speed_prim_direct = speed/3*2
-	speed_sec_direct = speed/3
+	speed = 5
 
 	keys = pygame.key.get_pressed()
-	if keys[K_w] and not keys[K_d]:
-		player = (player[0] + speed_prim_direct * dt, player[1] - speed_sec_direct * dt, player[2])
-		current = ghostE
-	elif (keys[K_w] and keys[K_d]):
-		player = (player[0] + speed * dt, player[1], player[2])
-		current = ghostSE
-	if keys[K_s] and not keys[K_a]:
-		player = (player[0] - speed_prim_direct * dt, player[1] + speed_sec_direct * dt, player[2])
-		current = ghostW
-	elif (keys[K_s] and keys[K_a]):
+	if keys[K_w]:
+		player = (player[0], player[1] - speed * dt, player[2])
+	if keys[K_s]:
+		player = (player[0], player[1] + speed * dt, player[2])
+	if keys[K_a]:
 		player = (player[0] - speed * dt, player[1], player[2])
-		current = ghostNW
-	if keys[K_a] and not keys[K_s]:
-		player = (player[0] + speed_prim_direct * dt * -1, player[1] - speed_sec_direct * dt, player[2])
-		current = ghostN
-	if keys[K_d] and not keys[K_w]:
-		player = (player[0] - speed_prim_direct * -1 * dt, player[1] + speed_sec_direct * dt, player[2])
-		current = ghostS
+	if keys[K_d]:
+		player = (player[0] + speed * dt, player[1], player[2])
 
 	for event in pygame.event.get():
 		if event.type == QUIT:
@@ -108,6 +105,5 @@ while True:
 
 	screen.blit(pygame.transform.scale(iso.display, res), (0, 0))
 
-	# print(int(clock.get_fps()))
 	screen.blit(font.render(f'FPS: {str(int(clock.get_fps()))}', False, (255, 255, 255)),(0,0))
 	pygame.display.update()
